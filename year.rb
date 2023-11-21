43325aae4090afcfcc22dc30dd6108fc7a882cd7
arthurfdiniz <artl-lur@hotmail.com>
2023-11-21 07:57:36 -0300

    first commit

--- year.rb
+++ year.rb
@@ -1,31 +1,31 @@
 result = ''
 
 loop do
-  puts result
-  puts 'MENU DO JOGO'
-  sleep (2)
-  puts 'Selecione 1 para continuar no jogo.'
-  sleep (2)
-  puts 'Selecione 0 para sair do jogo'
-  sleep (2)
-  print 'opção: '
+    puts result
+    puts 'MENU DO JOGO'
+    sleep (1)
+    puts 'Selecione 1 para continuar no jogo.'
+    sleep (1)
+    puts 'Selecione 0 para sair do jogo'
+    sleep (1)
+    print 'Opção: '
 
-option = gets.chomp.to_i
+  option = gets.chomp.to_i
 
-if option == 1
+  if option == 1
 
-  puts 'Digite o ano de nascimento: '
-  nascimento = gets.chomp.to_i
-  sleep (2)
-  puts 'Digite o ano atual: '
-  anoatual = gets.chomp.to_i
-  sleep (2)
-  idade = anoatual - nascimento
-  puts "Quem nasceu no ano de #{nascimento} hoje no ano de #{anoatual} terá #{idade}" 
-elsif option == 0
-  break 
-else
-  result = "Opção inválida"  
-end
-system "clear"
+    puts 'Digite o ano de nascimento: '
+    nascimento = gets.chomp.to_i
+    sleep (1)
+    puts 'Digite o ano atual: '
+    anoatual = gets.chomp.to_i
+    sleep (1)
+    idade = anoatual - nascimento
+    result = "Quem nasceu no ano de #{nascimento} hoje no ano de #{anoatual} terá #{idade}"
+  elsif option == 0
+    break
+  else
+    result = "Opção inválida"
+    end
+ system "clear"
 end
